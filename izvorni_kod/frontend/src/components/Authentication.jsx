169486d12e859d030a0fee0fefbd43ec68ac93ab
markoklicek <marko.klicek@fer.hr>
2024-11-08 18:56:47 +0100

    Dodao ikone na header i postavio gumbe za login i register

---
+++ izvorni_kod/frontend/src/components/Authentication.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Authentication.css';
+
+function Authentication() {
+    
+    return(
+        <div className='auth-container'>
+            <button className='login-button'>Login</button>
+            <button className='register-button'>Register</button>
+        </div>
+        
+    );
+}
+
+export default Authentication
--- izvorni_kod/frontend/src/components/Header.jsx
+++ izvorni_kod/frontend/src/components/Header.jsx
@@ -1,13 +1,27 @@
-import './Header.css';
-import logo from '../pictures/logo.png';
+import "./Header.css";
+import React, { useState } from "react";
+import Authentication from "./Authentication";
+import logo from "../pictures/logo.png";
+import user from "../pictures/user.png";
+import menu from "../pictures/menu.png";
 
+function Header() {
+  const [showAuth, setShowAuth] = useState(false);
 
-function Header(){
-    return (
-        <header>
-            <img src={logo} alt="logo"/>
-        </header>
-    );
+  const toggleAuth = () => setShowAuth((prev) => !prev);
+
+  return (
+    <div className="container">
+      <header className="header-container">
+        <img src={menu} alt="menu" />
+        <img src={logo} alt="logo" />
+        <img src={user} alt="user" onClick={toggleAuth} />
+      </header>
+      <div className="auth-container">
+        {showAuth && <Authentication />}
+        </div>
+    </div>
+  );
 }
 
 export default Header
