d54f51676a8374cfc493d94aa7414de9439af378
Antonio Macan <antonio.macan@fer.unizg.hr>
2025-01-21 00:36:38 +0100

    update settings

--- izvorni_kod/backend/backend/urls.py
+++ izvorni_kod/backend/backend/urls.py
@@ -1,35 +1,40 @@
+"""
+URL configuration for backend project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
-import os
 
 from api.views import custom_admin_logout
 
-def debug_media(request, path):
-    full_path = os.path.join(settings.MEDIA_ROOT, path)
-    exists = os.path.exists(full_path)
-    media_root = str(settings.MEDIA_ROOT)
-    files_in_dir = os.listdir(os.path.dirname(full_path)) if exists else []
-    
-    return HttpResponse(
-        f"Looking for: {full_path}\n"
-        f"File exists: {exists}\n"
-        f"MEDIA_ROOT: {media_root}\n"
-        f"Files in directory: {files_in_dir}"
-    )
+from django.http import FileResponse, Http404
+import os
+
+def serve_media_file(request, path):
+    file_path = os.path.join(settings.MEDIA_ROOT, path)
+    if os.path.exists(file_path):
+        return FileResponse(open(file_path, 'rb'))
+    raise Http404("File not found")
 
 urlpatterns = [
+    # Override the default admin logout path
     path('admin/logout/', custom_admin_logout, name='admin-logout'),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('debug-media/<path:path>', debug_media),  # Dodajte ovu liniju
+    path('media/<path:path>', serve_media_file),  # Dodajte ovu liniju
 ]
 
-if not settings.DEBUG:
-    urlpatterns += [
-        path('media/<path:path>', debug_media),  # Privremeno zamijenite standardno posluživanje s debug viewom
-    ]
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
